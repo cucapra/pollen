8905077c0aeae6208a9021207a036bd03710ae60
David Chen <dc854@cornell.edu>
2022-04-12 16:28:24 -0400

    seperated out name look up for segments

--- depth2.py
+++ depth2.py
@@ -9,17 +9,21 @@ s_names = g['segment names']
 s_map = {}
 s_counter = 0
 
-depth_map = {}
+result_map = {}
 
 for s in s_names:
     s_map[s_counter] = s
 
 for i in range(len(p_offsets) - 1):
     for j in range(p_offsets[i], p_offsets[i + 1]):
-        name = s_names[paths[j]]
-        if name in depth_map:
-            depth_map[name] += 1
+        index = paths[j]
+        if index in result_map:
+            result_map[index] += 1
         else:
-            depth_map[name] = 1
+            result_map[index] = 1
+
+depth_map = {}
+for k, v in result_map.items():
+    depth_map[s_names[k]] = v
 
 print(depth_map)
