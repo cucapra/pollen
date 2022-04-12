a1b8b343cebc6c806fea4bfb0deabd79387493c7
David Chen <dc854@cornell.edu>
2022-04-12 16:33:30 -0400

    changed dictionary to list in hardware side implementation

--- depth2.py
+++ depth2.py
@@ -6,24 +6,17 @@ with open('k.json') as f:
 p_offsets = g['path offsets']
 paths = g['paths']
 s_names = g['segment names']
-s_map = {}
 s_counter = 0
 
-result_map = {}
-
-for s in s_names:
-    s_map[s_counter] = s
+counts = [0] * len(s_names)
 
 for i in range(len(p_offsets) - 1):
     for j in range(p_offsets[i], p_offsets[i + 1]):
         index = paths[j]
-        if index in result_map:
-            result_map[index] += 1
-        else:
-            result_map[index] = 1
+        counts[index] += 1
 
 depth_map = {}
-for k, v in result_map.items():
-    depth_map[s_names[k]] = v
+for i in range(len(counts)):
+    depth_map[s_names[i]] = counts[i]
 
 print(depth_map)
