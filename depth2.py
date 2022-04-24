4cebff2a83a669761b633e78541d7850d3608744
David Chen <dc854@cornell.edu>
2022-04-24 18:55:26 -0400

    parsing files without gfapy

--- depth2.py
+++ depth2.py
@@ -1,22 +1,39 @@
 import json
+import sys
+import format_gfa
+import os
 
-with open('k.json') as f:
-    g = json.load(f)
+def depth2(filename):
+    with open(filename) as f:
+        g = json.load(f)
+    p_offsets = g['path offsets']
+    paths = g['paths']
+    s_names = g['segment names']
 
-p_offsets = g['path offsets']
-paths = g['paths']
-s_names = g['segment names']
-s_counter = 0
+    counts = [0] * len(s_names)
 
-counts = [0] * len(s_names)
+    for i in range(len(p_offsets) - 1):
+        for j in range(p_offsets[i], p_offsets[i + 1]):
+            index = paths[j]
+            counts[index] += 1
 
-for i in range(len(p_offsets) - 1):
-    for j in range(p_offsets[i], p_offsets[i + 1]):
-        index = paths[j]
-        counts[index] += 1
+    depth_map = {}
+    for i in range(len(counts)):
+        depth_map[s_names[i]] = counts[i]
 
-depth_map = {}
-for i in range(len(counts)):
-    depth_map[s_names[i]] = counts[i]
+    print(depth_map)
 
-print(depth_map)
+def depth2_nojson(filename):
+    format_gfa.generate_json(filename)
+    json_file = os.path.splitext(filename)[0] + '.json'
+    depth2(json_file)
+
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    ext = os.path.splitext(filename)[1]
+    if ext == '.json':
+        depth2(filename)
+    elif ext == '.gfa':
+        depth2_nojson(filename)
+    else:
+        raise Exception("Invalid file format.")
--- format_gfa.py
+++ format_gfa.py
@@ -1,35 +1,57 @@
 import json
+import os
 
-f = open('k.gfa', 'r')
-lines = f.readlines()
+def format_file(filename):
+    f = open(filename, 'r')
+    lines = f.readlines()
 
-s_names = []
-s_map = {}
-s_counter = 0
+    s_names = []
+    s_map = {}
+    s_counter = 0
 
-p_names = []
-p_offset = 0
-p_offsets = []
-paths = []
+    p_names = []
+    p_offset = 0
+    p_offsets = []
+    paths = []
 
-for line in lines:
-    split = line.split()
-    if split[0] == 'S':
-        s_name = split[1]
-        s_names.append(s_name)
-        s_map[s_name] = s_counter
-        s_counter += 1
-    if split[0] == 'P':
-        p_names.append(split[1])
-        path_segments = split[2].split(',')
-        p_offsets.append(p_offset)
-        for segment in path_segments:
-            segment = segment.rstrip('+-')
-            paths.append(s_map[segment])
-            p_offset += 1
-p_offsets.append(len(paths))
+    for line in lines:
+        split = line.split()
+        if split[0] == 'S':
+            s_name = split[1]
+            s_names.append(s_name)
+            s_map[s_name] = s_counter
+            s_counter += 1
+        if split[0] == 'P':
+            p_names.append(split[1])
+            path_segments = split[2].split(',')
+            p_offsets.append(p_offset)
+            for segment in path_segments:
+                segment = segment.rstrip('+-')
+                paths.append(s_map[segment])
+                p_offset += 1
+    p_offsets.append(len(paths))
 
-result_dict = {"segment names": s_names, "paths": paths, 
+    result_dict = {"segment names": s_names, "paths": paths, 
     "path offsets": p_offsets, "path names": p_names}
-with open("k.json", "w") as o:
-    json.dump(result_dict, o)
+    return result_dict
+
+def format_file_path_only(filename):
+    f = open(filename, 'r')
+    lines = f.readlines()
+    all_paths = []
+
+    for line in lines:
+        split = line.split()
+        if split[0] == 'P':
+            path_segments = split[2].split(',')
+            for segment in path_segments:
+                segment = segment.rstrip('+-')
+                all_paths.append(segment)
+    
+    return all_paths
+
+def generate_json(filename):
+    result_dict = format_file(filename)
+    json_file = os.path.splitext(filename)[0] + '.json'
+    with open(json_file, "w") as o:
+        json.dump(result_dict, o)
