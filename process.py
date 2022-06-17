0d08464612c325e9c23898fc998073e49337a2db
susan-garry <sgarry406@gmail.com>
2022-06-17 15:26:09 -0400

    process.py can now format calyx output

--- process.py
+++ process.py
@@ -1,8 +1,30 @@
+import json
 import sys
 
-'''
-Reads a graph depth table from the commandline and removes the depth.uniq column
-'''
-node_depths = sys.stdin.readlines()
-for row in node_depths:
-    print(row[:row.rfind('\t')])
+
+def format_graph_depth_table(node_depths):
+    '''
+    Reads a graph depth table from the commandline and removes the depth.uniq column
+    '''
+    for row in node_depths:
+        print(row[:row.rfind('\t')])
+
+def format_json_data(node_depths, mem='segments0'):
+    '''
+    Takes a json data file (calyx output) and formats it as above
+    '''
+    depths = node_depths['memories'][mem]
+    print('#node.id\tdepth')
+    for i in range(len(depths)):
+        print(f'{i}\t{depths[i]}')
+    
+if __name__ == '__main__':
+    '''
+    Take a commandline arg, gdt or json, to specify which file to convert
+    '''
+    format = sys.argv[1]
+    if format == "gdt":
+        format_graph_depth_table(sys.stdin.readlines())
+    elif format == "json":
+        data = json.load(sys.stdin)
+        format_json_data(data)
