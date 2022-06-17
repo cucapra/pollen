4f2f651d696ba5a1f4e99a0e6ec877ba6f2c1499
susan-garry <sgarry406@gmail.com>
2022-06-17 14:00:03 -0400

    clean up repo, use turnt for testing

--- process.py
+++ process.py
@@ -1,11 +1,8 @@
 import sys
 
-with open(sys.argv[1], 'r') as f: 
-    contents = f.readlines()
-    data = []
-    for line in contents:
-        data.append(line.split())
-
-with open('odgi_output.txt', 'w') as f2:
-    for i in range(1, len(data)):
-        f2.write(f'{data[i][0].strip()} {data[i][1].strip()}\n')
+'''
+Reads a graph depth table from the commandline and removes the depth.uniq column
+'''
+node_depths = sys.stdin.readlines()
+for row in node_depths:
+    print(row[:row.rfind('\t')])
