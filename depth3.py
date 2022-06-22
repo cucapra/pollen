4e6ec44bda54a5df72473d44f2b0e608ead7c8bd
susan-garry <sgarry406@gmail.com>
2022-06-22 18:12:02 -0400

    add support for only considering a subset of the graphs

--- depth3.py
+++ depth3.py
@@ -1,8 +1,7 @@
-# NOTE: Code has not been tested because of issues importing odgi
 import sys
 import odgi
 
-def get_depth_table(graph):
+def get_depth_table(graph, subset_paths=[]):
     ''' 
     Input: an odgi.graph object
     Output: a dictionary that maps from a node_id to (node.depth, node.uniq),
@@ -11,6 +10,12 @@ def get_depth_table(graph):
     '''
 
     ndt = dict()
+    
+    if subset_paths: # Consder only paths specified, if specified
+        paths_to_consider = set(subset_paths)
+    else: # By default, we want to consider all paths
+        paths_to_consider = set()
+        graph.for_each_path_handle(lambda h: paths_to_consider.add(graph.get_path_name(h)))
     
     def get_node_depth(handle):
         '''
@@ -22,8 +27,10 @@ def get_depth_table(graph):
         depth = [0]
         
         def for_step(step):
-            paths.add(graph.get_path_handle_of_step(step))
-            depth[0] = depth[0] + 1
+            path_h = graph.get_path_handle_of_step(step)
+            if graph.get_path_name(path_h) in paths_to_consider:
+                paths.add(path_h)
+                depth[0] = depth[0] + 1
 
         graph.for_each_step_on_handle(handle, for_step)
 
