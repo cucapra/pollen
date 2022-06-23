732044356d20a969752119aab82052b4d7d7754b
susan-garry <sgarry406@gmail.com>
2022-06-23 13:37:28 -0400

    fix bug in depth3.py when calculating depth.uniq

--- parse_data.py
+++ parse_data.py
@@ -2,6 +2,9 @@ import sys
 import json
 import odgi
 
+MAX_NUM_PATHS=12
+MAX_NODE_DEPTH=12
+
 def node_depth_odgi(filename, paths_to_consider=[]):
     '''
     Parse an odgi file for the calyx node depth algorithm
@@ -11,14 +14,15 @@ def node_depth_odgi(filename, paths_to_consider=[]):
     shift = graph.min_node_id()
     data = {}
 
-    # Obtain a sorted list of path names. A path's position on this list is its id
+    # Obtain a sorted list of path names. A path's index + 1 is its id
+    # Note: path id's are 1 - indexed because 0 indicates the absense of a path
     paths_set = set()
     # Add the name of each path to [paths]
     graph.for_each_path_handle(lambda h: paths_set.add(graph.get_path_name(h)))
     # Convert paths to a sorted list
     paths_list = sorted(paths_set)
     # Path name -> path id
-    paths = {path:count for count, path in enumerate(paths_list)}
+    paths = {path:count for count, path in enumerate(paths_list, start=1)}
 
     # Initialize the data for each node
     def parse_node(node_h):
@@ -36,6 +40,10 @@ def node_depth_odgi(filename, paths_to_consider=[]):
             path_ids.append(path_id)
             
         graph.for_each_step_on_handle(node_h, parse_step)
+
+        # Pad path_ids with 0s
+        path_ids = path_ids + [0] * (MAX_NUM_PATHS + 1 - len(path_ids))
+        
         # 'path_ids{id}' is the list of path ids for each step crossing node {id}
         data[f'path_ids{node_id - shift}'] = {
             "data": path_ids,
@@ -48,9 +56,12 @@ def node_depth_odgi(filename, paths_to_consider=[]):
 
     graph.for_each_handle(parse_node)
 
+    # TODO: pad data with 0s for absent nodes?
+
     # Initialize the memory for the subset of paths to consider
+    consider_paths = [0] * MAX_NUM_PATHS
+
     if paths_to_consider:
-        consider_paths = [0] * graph.get_path_count()
         for path in paths_to_consider:
             path_id = paths[path]
             consider_paths[path_id] = 1
@@ -63,8 +74,9 @@ def node_depth_odgi(filename, paths_to_consider=[]):
                 "width": 1
             }
         }
-    else:
-        consider_paths = [1] * graph.get_path_count()
+    else: # By default, all paths are initialized to 1
+        for i in range(len(graph.get_path_count())):
+            consider_paths[i] = 1
         
     return data
 
