a77d27c0c9d19fde64bcaecbe9c0521c2fbc11b7
susan-garry <sgarry406@gmail.com>
2022-06-24 17:57:03 -0400

    python depth algo now accepts subset of paths from command line, added testing functionality for subset of paths

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 TEST_FILES := t k note5 overlap q.chop DRB1-3123 chr6.C4 LPA
-BASIC_TEST_FILES := ex1 ex2
+BASIC_TESTS := ex1 ex2
 GFA_URL := https://raw.githubusercontent.com/pangenome/odgi/ebc493f2622f49f1e67c63c1935d68967cd16d85/test
 GFA_ZIP_URL := https://s3-us-west-2.amazonaws.com/human-pangenomics/pangenomes/scratch/2021_05_06_pggb/gfas/chr8.pan.gfa.gz
 
@@ -14,14 +14,20 @@ test: og
 	-turnt --save --env baseline test/basic/*.gfa
 	turnt test/basic/*.gfa
 
+	-turnt --save --env baseline test/subset-paths/*.txt
+	-turnt test/subset-paths/*.txt
+
 test-all: GFA_ZIP_URL test/chr8.pan.og test
 	-turnt --save --env baseline test/chr8.pan.gfa
 	turnt test/chr8.pan.gfa
 
 clean:
 	rm -rf $(foreach file,$(TEST_FILES),test/$(file).*)
+
 	rm -rf test/basic/*.og
 	rm -rf test/basic/*.out
+
+	rm -rf test/subset-paths/*.out
 
 GFA_ZIP:
 	curl -Lo ./test/chr8.pan.gfa.gz $(GFA_ZIP_URL)
--- depth.py
+++ depth.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 import odgi
 ''' 
@@ -5,7 +6,7 @@ The documentation for the odgi module can be found at
 https://odgi.readthedocs.io/en/latest/rst/binding/glossary.html 
 '''
 
-def get_depth_table(graph, subset_paths=[]):
+def get_depth_table(graph, subset_paths=None):
     ''' 
     Input: an odgi.graph object
     Output: the node depth table, a dictionary that maps from a node's id to it's (depth, uniq_depth),
@@ -17,11 +18,12 @@ def get_depth_table(graph, subset_paths=[]):
     ndt = dict() # node depth table map from node.id -> (node.depth, node.uniq_depth)
 
     # Compute the set of paths we want to consider
-    if subset_paths: # Consder only paths specified, if specified
-        paths_to_consider = set(subset_paths)
-    else: # By default, we want to consider all paths
+    if subset_paths is None: # By default, we want to consider all paths
         paths_to_consider = set()
+        # Adds the name of each path in graph to paths_to_consider
         graph.for_each_path_handle(lambda h: paths_to_consider.add(graph.get_path_name(h)))
+    else: # Consider only paths specified, if a subet of paths is specified
+        paths_to_consider = set(subset_paths)
 
     # Compute the node depth and unique depth
     def get_node_depth(handle):
@@ -55,10 +57,34 @@ def get_depth_table(graph, subset_paths=[]):
     graph.for_each_handle(get_node_depth)
     return ndt
 
+def parse_paths_file(filename):
+    ''' Parse a file which contains the name of a path on each line. '''
+
+    if filename is None: # Return the default value
+        return None
+    
+    with open(filename, 'r') as paths_file:
+        text = paths_file.read()
+        paths = text.splitlines()
+    return paths
+
 if __name__ == '__main__':
+    # Parse commandline arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename', help='A .og file representing a pangenome whose node depth we want to calculate')
+    parser.add_argument('-s', '--subset-paths', help='Specify a file containing a subset of all paths in the graph. See the odgi documentation for more details')
+    args = parser.parse_args()
+
     graph = odgi.graph()
-    graph.load(sys.argv[1])
-    ndt = get_depth_table(graph)
+    graph.load(args.filename)
+
+    # Get the set of all paths specified in the file give
+    subset_paths = parse_paths_file(args.subset_paths)
+    
+    # Get the node depths for all nodes in the graph
+    ndt = get_depth_table(graph, subset_paths)
+
+    # Print the ndt to the standard output
     print("#node.id\tdepth\tdepth.uniq")
     for id, (depth, uniq) in sorted(ndt.items()):
         print(f'{id}\t{depth}\t{uniq}')
