e541d0c22306aaf5c2fe6dfdbaa41b66b573a234
David Chen <dc854@havarti.cs.cornell.edu>
2022-11-28 17:49:56 -0500

    new degree impl

--- pollen/depth/parse_data.py
+++ pollen/depth/parse_data.py
@@ -11,6 +11,7 @@ import odgi
 MAX_NODES=16
 MAX_STEPS=15
 MAX_PATHS=15
+MAX_EDGES=16
 
 def parse_odgi(filename, subset_paths, max_nodes, max_steps, max_paths):
     '''
@@ -52,6 +53,94 @@ def parse_odgi(filename, subset_paths, max_nodes, max_steps, max_paths):
 
     return data
     
+def parse_edges_on_nodes(graph, path_name_to_id, max_nodes):
+    '''
+    Generate input data containing the path ids for each edge for the nodes on the graph
+    {path_ids1: 
+        "data": [1, 1, 2, 0],
+            "format": {
+                "numeric_type": "bitnum",8kklkskl
+                "is_signed": False,
+                "width": 2
+            }
+    }
+    '''
+    num_nodes = graph.get_node_count()
+    
+    # Check that the number of steps on the node does not exceed max_steps
+    if num_nodes > max_nodes:
+        raise Exception(f'The number of nodes in the graph exceeds the maximum number of nodes the hardware can process. Hint: try setting the maximum number of nodes manually using the -n flag.')
+    
+    data = {}
+    width = max_paths.bit_length()
+
+    # Initialize the data for each node
+    def parse_node(node_h):
+        '''
+        Get a list of path ids for each step on node_h.
+        '''
+
+        # Check that the number of steps on the node does not exceed max_steps
+        if graph.get_step_count(node_h) > max_steps:
+            raise Exception(f'The number of paths in the graph exceeds the maximum number of paths the hardware can process. {graph.get_step_count(node_h)} > {max_steps}. Hint: try setting the maximum number of steps manually using the -e flag.')
+        
+        path_ids = []
+
+        def parse_step(step_h):
+            path_h = graph.get_path(step_h)
+            path_id = path_name_to_id[graph.get_path_name(path_h)]
+            path_ids.append(path_id)
+            
+        graph.for_each_step_on_handle(node_h, parse_step)
+
+        # Pad path_ids with 0s
+        path_ids = path_ids + [0] * (max_steps - len(path_ids))
+        
+        # 'path_ids{id}' is the list of path ids for each step crossing node {id}
+        node_id = graph.get_id(node_h)
+        data[f'path_ids{node_id}'] = {
+            "data": path_ids,
+            "format": {
+                "numeric_type": "bitnum",
+                "is_signed": False,
+                "width": width
+            }
+        }
+
+    graph.for_each_handle(parse_node)
+
+    default_steps = [0] * max_steps
+    
+    while num_nodes < max_nodes:
+        num_nodes += 1
+        data[f'path_ids{num_nodes}'] = {
+            "data": default_steps,
+            "format": {
+                "numeric_type": "bitnum",
+                "is_signed": False,
+                "width": width
+            }
+        }
+
+    data['depth_output'] = {
+        "data": [0] * max_nodes,
+        "format": {
+            "numeric_type": "bitnum",
+            "is_signed": False,
+            "width": max_steps.bit_length()
+        }
+    }
+
+    data['uniq_output'] = {
+        "data": [0] * max_nodes,
+        "format": {
+            "numeric_type": "bitnum",
+            "is_signed": False,
+            "width": max_paths.bit_length()
+        }
+    }
+        
+    return data
 
 def parse_steps_on_nodes(graph, path_name_to_id, max_nodes, max_steps, max_paths):
     '''
---
+++ pollen/depth/python_degree.py
@@ -0,0 +1,83 @@
+""" A node degree computation for .og files implemented using odgi's
+Python bindings. While this implementation reuses odgi's data structures, it
+does not reuse its node degree computation algorithm and instead implements
+it from scratch.
+
+The documentation for the odgi module can be found at 
+https://odgi.readthedocs.io/en/latest/rst/binding/glossary.html 
+"""
+
+import argparse
+import sys
+import odgi
+
+def get_degree_table(graph):
+    ''' 
+    Input: an odgi.graph object
+    Output: the node degree table, a dictionary that maps from a node's id to its (degree, uniq_degree),
+        where degree is the total number of times each path in subset_paths crosses the node,
+        and uniq_degree is the number of paths in subset_paths which cross the node
+    Note: if subset_paths is empty, consider all paths when computing node degree
+    '''
+
+    ndt = dict() # node degree table map from node.id -> (node.degree)
+
+    # Compute the node degree and unique degree
+    def get_node_degree(handle):
+        '''
+        Input: [handle] is an odgi.handle object which represents a node
+        Inserts node.degree and node.uniq into ndt for the node associated with
+            [handle]
+        '''
+
+        # Note: a node can have multiple handles, but only one id
+        node_id = graph.get_id(handle)
+        degree = 0
+
+        def sum_edges(node):
+            nonlocal degree
+            degree += 1
+        graph.follow_edges(handle, False, sum_edges)
+        graph.follow_edges(handle, True, sum_edges)
+
+        ndt[node_id] = ndt.get(node_id, 0) + degree
+        
+    graph.for_each_handle(get_node_degree)
+    return ndt
+
+def parse_paths_file(filename):
+    ''' Parse a file which contains the name of a path on each line. '''
+
+    if filename is None: # Return the default value
+        return None
+    
+    with open(filename, 'r') as paths_file:
+        text = paths_file.read()
+        paths = text.splitlines()
+    return paths
+
+if __name__ == '__main__':
+    # Parse commandline arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename', help='A .og file representing a pangenome whose node degree we want to calculate')
+    args = parser.parse_args()
+
+    graph = odgi.graph()
+    graph.load(args.filename)
+    
+    # Get the node degrees for all nodes in the graph
+    ndt = get_degree_table(graph)
+
+    # Print the ndt to the standard output
+    print("#node.id\tdegree")
+    for id, degree in sorted(ndt.items()):
+        print(f'{id}\t{degree}')
+
+'''
+import odgi
+import python_degree
+graph = odgi.graph()
+name = './processing-elements/test/k.og'
+graph.load(name)
+python_degree.get_degree_table(graph)
+'''
