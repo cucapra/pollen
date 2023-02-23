c0f216260dcdd08bbec9264a893132d092d854e5
Anshuman Mohan <anshumanmohan@live.com>
2023-02-22 22:33:46 -0800

    Implement node degree. Creates a preprocessed table that may be a more general version of node_step

--- slow_odgi/preprocess.py
+++ slow_odgi/preprocess.py
@@ -29,7 +29,6 @@ def crush_n_graph(graph):
 def node_steps(graph):
     """For each segment in the graph,
        list the times the segment was crossed by a path"""
-
     # segment name, (path name, index on path, direction) list
     crossings: Dict[str, List[Tuple[str, int, bool]]] = {}
     for segment in graph.segments.values():
