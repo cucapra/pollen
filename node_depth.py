75f38c6b610cf5e3674cb6d8e778a669ea9d150c
Anshuman Mohan <anshumanmohan@live.com>
2023-02-22 16:18:16 -0800

    Normalize the output format of `depth`. Add depth.unique, but not subset_paths (yet)

--- node_depth.py
+++ node_depth.py
@@ -5,8 +5,12 @@ import preprocess
 
 def node_depth(graph):
     # The depth of a node is just the cardinality of node_step for that node
-    for (segment, crossings) in preprocess.node_steps(graph).items():
-        print('\t'.join([segment, str(len(crossings))]))
+    print('\t'.join(["node.id", "depth", "depth.uniq"]))
+    for (seg, crossings) in preprocess.node_steps(graph).items():
+        # for depth.uniq,
+        # we just need to know how many unique path-names there are
+        uniq_path_names = set(c[0] for c in crossings)
+        print('\t'.join([seg, str(len(crossings)), str(len(uniq_path_names))]))
 
 
 if __name__ == "__main__":
