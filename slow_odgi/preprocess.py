4b672895dde45101948e3e55814f74abbc65495e
anshumanmohan <anshumanmohan@live.com>
2023-03-17 18:14:18 -0400

    lift node degree's preprocessing

--- slow_odgi/preprocess.py
+++ slow_odgi/preprocess.py
@@ -15,3 +15,23 @@ def node_steps(graph):
             crossings[seg_name].append((path.name, id, seg_orient))
 
     return crossings
+
+def in_out(graph):
+    """
+    key: segment name i.e. me
+    value: list of (segment name i.e. you, edge_is_from_me_to_you)
+
+    We take each step into account, regardless of whether it is on a path.
+    We could add a further, optional, item to the key with which to indicate
+    whether the link is on a path.
+    """
+    in_out: Dict[str, List[Tuple[str, bool]]] = {}
+    for segment in graph.segments.values():
+        in_out[segment.name] = []
+
+    for link in graph.links:
+        in_out[link.from_].append((link.to, True))
+        # Can add this symmetric information separately...
+        in_out[link.to].append((link.from_, False))
+
+    return in_out
