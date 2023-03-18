b1f296fbb0cb9f443a18f78ea1d850a2737d0399
anshumanmohan <anshumanmohan@live.com>
2023-03-18 17:42:06 -0400

    further cleanup in preprocess

--- slow_odgi/node_degree.py
+++ slow_odgi/node_degree.py
@@ -6,8 +6,12 @@ from typing import List, Tuple, Dict
 def node_degree(graph):
     # The degree of a node is just the cardinality of in_out for that node
     print('\t'.join(["#node.id", "node.degree"]))
-    for (seg, in_out) in preprocess.in_out(graph).items():
-        print('\t'.join([seg, str(len(in_out))]))
+    ins, outs = preprocess.in_out_edges(graph)
+    for seg in graph.segments.values():
+        seg = seg.name
+        out_degree = len(outs[(seg, True)]) + len(outs[(seg, False)])
+        in_degree = len(ins[(seg, True)]) + len(ins[(seg, False)])
+        print('\t'.join([seg, str(in_degree + out_degree)]))
 
 
 if __name__ == "__main__":
