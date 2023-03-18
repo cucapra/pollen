3791dfa9aa54afec1910449ad8a9e14587ac67b0
anshumanmohan <anshumanmohan@live.com>
2023-03-18 19:32:42 -0400

    fix up matrix to use new precomputation

--- slow_odgi/matrix.py
+++ slow_odgi/matrix.py
@@ -6,11 +6,11 @@ from typing import List, Tuple, Dict
 def matrix(graph):
 	topseg = max([int(i) for i in graph.segments.keys()])
 	print(" ".join(str(i) for i in [topseg, topseg, 2*len(graph.links)]))
-	for (seg, neighbors) in preprocess.in_out(graph).items():
-		dst_neighbors = filter (lambda x: x[1], neighbors)
-		for dst_neighbor in dst_neighbors:
-				print(" ".join([seg, dst_neighbor[0], "1"]))
-				print(" ".join([dst_neighbor[0], seg, "1"]))
+	_, outs = preprocess.in_out_edges(graph)
+	for (seg, neighbors) in outs.items():
+		for neighbor in neighbors:
+			print(" ".join([seg[0], neighbor[0], "1"]))
+			print(" ".join([neighbor[0], seg[0], "1"]))
 
 if __name__ == "__main__":
     name = sys.stdin
