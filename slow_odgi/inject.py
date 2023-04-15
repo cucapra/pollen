23777b964c6badfae3c89d7aacfb6135ea75235a
anshumanmohan <anshumanmohan@live.com>
2023-04-15 17:22:00 -0400

    Progress towards more general solution

--- slow_odgi/inject.py
+++ slow_odgi/inject.py
@@ -15,7 +15,7 @@ def track_path(graph, bed):
   for handle in graph.paths[bed.name].segments:
     length = len(graph.segments[handle.name].seq)
     if (walk < bed.lo):
-      walk = walk + length
+      walk = walk + length # Skipping over segments that are not of interest.
       continue
     if (walk + length <= bed.hi):
       walk = walk + length
@@ -25,11 +25,41 @@ def track_path(graph, bed):
   # Given a legal BED request, I should never reach this point.
 
 
+def where_chop(graph, pathname, index):
+  """Given a path and an index, find which segment should be cut.
+  It's possible we won't need to cut: the index could be at a seam b/w segments.
+  In such case, return None
+  """
+  walk = 0
+  for handle in graph.paths[pathname].segments:
+    if (walk == index):
+      return None
+    if (walk > index):
+      return handle.name, walk - index
+    walk = walk + len(graph.segments[handle.name].seq)
+
+
+def chop_if_needed(graph, pathname, index):
+  """Modify this graph such that the given index will fall on a segment-seam.
+  This involves:
+    1. renumbering segments
+    2. redoing paths
+  But at least we know we'll only ever need to renumber a max of one segment.
+  """
+  segpos = where_chop(graph, pathname, index)
+  if not segpos:
+    return graph # We were already on a seam.
+  segname, pos = segpos
+  print(f"Not implemented: need to chop seg {segname} at position {pos}")
+  return graph
+
+
 def inject_paths(graph, p2i):
   """Given a graph and the list of paths to inject, inject those paths."""
   newpaths = {}
   for p in p2i:
     if p.name in graph.paths.keys(): # odgi is silent if name was invalid
+      graph = chop_if_needed(chop_if_needed(graph, p.name, p.lo), p.name, p.hi)
       new_segs = track_path(graph, p)
       newpaths[p.new] = mygfa.Path(p.new, new_segs, None)
   paths = graph.paths | newpaths
