4f53b9550cd2195d2ac34da3b0126bcd7d938295
anshumanmohan <anshumanmohan@live.com>
2023-04-26 19:03:46 -0400

    Infer parameters automatically

--- slow_odgi/slow_odgi/mygfa.py
+++ slow_odgi/slow_odgi/mygfa.py
@@ -225,7 +225,8 @@ class Graph:
         if showlinks:
             for link in sorted(self.links):
                 print(str(link), file=outfile)
-                
+
+
 if __name__ == "__main__":
     graph = Graph.parse(sys.stdin)
     if len(sys.argv) > 1 and sys.argv[1] == "--nl":
