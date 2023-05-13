eeaaa056f16b908794024136679516de68c8db27
Adrian Sampson <adrian@radbox.org>
2023-05-13 15:59:07 -0400

    Start docs for mygfa

---
+++ mygfa/docs/conf.py
@@ -0,0 +1,12 @@
+project = 'mygfa'
+copyright = '2023, Cornell University'
+author = 'Cornell University'
+release = '0.1'
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
