db5d7a2cae6cfa5846e4df5f4907d78ef73453b5
anshumanmohan <anshumanmohan@live.com>
2023-06-05 14:44:31 -0400

    Simplify JSON output of headers

--- pollen_data_gen/pollen_data_gen/simple.py
+++ pollen_data_gen/pollen_data_gen/simple.py
@@ -14,8 +14,11 @@ class GenericSimpleEncoder(JSONEncoder):
     def default(self, o: Any) -> SimpleType:
         if isinstance(o, mygfa.Path):
             items = str(o).split("\t")
+            # We can drop the 0th cell, which will just be 'P',
+            # and the 1st cell, which will just be the path's name.
             return {"segments": items[2], "overlaps": items[3]}
         if isinstance(o, mygfa.Link):
+            # We perform a little flattening.
             return {
                 "from": o.from_.name,
                 "from_orient": o.from_.ori,
@@ -23,7 +26,10 @@ class GenericSimpleEncoder(JSONEncoder):
                 "to_orient": o.to_.ori,
                 "overlap": str(o.overlap),
             }
-        if isinstance(o, (mygfa.Segment, mygfa.Alignment, mygfa.Header, mygfa.Link)):
+        if isinstance(o, mygfa.Header):
+            # We can flatten the header objects into a simple list of strings.
+            return str(o)
+        if isinstance(o, (mygfa.Segment, mygfa.Alignment, mygfa.Link)):
             return dataclasses.asdict(o)
         return None
 
