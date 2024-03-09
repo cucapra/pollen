ef68a562f7dc31b0d2b8ce6bdf8b05ca10e30a9d
Adrian Sampson <adrian@radbox.org>
2024-03-09 18:23:02 -0500

    Try printing out paths??

--- polbin/src/main.rs
+++ polbin/src/main.rs
@@ -8,4 +8,11 @@ fn main() {
     for seg in &flat.segs {
         println!("S\t{}\t{}", seg.name, flat.get_seq(seg));
     }
+    for path in &flat.paths {
+        print!("P\t{}\t", path.name);
+        for step in flat.get_steps(path) {
+            print!("{}{}", step.segment, if step.forward { "+" } else { "-" });
+        }
+        println!();
+    }
 }
