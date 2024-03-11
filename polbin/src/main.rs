7233dc81bfb1e4e055736778fa86f5087175a2d5
Adrian Sampson <adrian@radbox.org>
2024-03-11 18:52:12 -0400

    Proper CLI option parsing

--- polbin/src/main.rs
+++ polbin/src/main.rs
@@ -2,6 +2,7 @@ mod file;
 mod flatgfa;
 mod parse;
 mod print;
+use argh::FromArgs;
 use memmap::{Mmap, MmapMut};
 
 fn map_file(name: &str) -> Mmap {
@@ -20,20 +21,42 @@ fn map_file_mut(name: &str) -> MmapMut {
     unsafe { MmapMut::map_mut(&file) }.unwrap()
 }
 
-fn main() {
-    // Read either GFA text from stdin or a binary file from the first argument.
-    if let Some(name) = std::env::args().nth(1) {
-        let mmap = map_file(&name);
-        let gfa = file::view(&mmap);
-        print::print(&gfa);
-    } else {
-        let stdin = std::io::stdin();
-        let store = parse::Parser::parse(stdin.lock());
-        let gfa = store.view();
-        print::print(&gfa);
+#[derive(FromArgs)]
+/// Convert between GFA text and FlatGFA binary formats.
+struct PolBin {
+    /// read from a binary FlatGFA file
+    #[argh(option, short = 'i')]
+    input: Option<String>,
 
-        // TODO Just try dumping to a file.
-        let mut mmap = map_file_mut("hello.flatgfa");
-        file::dump(&gfa, &mut mmap);
+    /// write to a binary FlatGFA file
+    #[argh(option, short = 'o')]
+    output: Option<String>,
+}
+
+fn main() {
+    let args: PolBin = argh::from_env();
+
+    // Load the input from a file (binary) or stdin (text).
+    let mmap;
+    let store;
+    let gfa = match args.input {
+        Some(name) => {
+            mmap = map_file(&name);
+            file::view(&mmap)
+        }
+        None => {
+            let stdin = std::io::stdin();
+            store = parse::Parser::parse(stdin.lock());
+            store.view()
+        }
+    };
+
+    // Write the output to a file (binary) or stdout (text).
+    match args.output {
+        Some(name) => {
+            let mut mmap = map_file_mut(&name);
+            file::dump(&gfa, &mut mmap);
+        }
+        None => print::print(&gfa),
     }
 }
