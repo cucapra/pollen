6b2df97226cfd6d2f349b4d77d9aea6f6e79932e
Adrian Sampson <adrian@radbox.org>
2024-03-24 10:13:25 -0400

    Black formatting for conf.py 🙄

--- mygfa/docs/conf.py
+++ mygfa/docs/conf.py
@@ -1,13 +1,14 @@
 # Make mygfa module available to autodoc.
 import sys
 import os
-sys.path.insert(0, os.path.abspath('..'))
 
-project = 'mygfa'
-copyright = '2024, Capra Lab'
-author = 'Capra Lab'
+sys.path.insert(0, os.path.abspath(".."))
 
-extensions = ['sphinx.ext.autodoc']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+project = "mygfa"
+copyright = "2024, Capra Lab"
+author = "Capra Lab"
 
-html_theme = 'alabaster'
+extensions = ["sphinx.ext.autodoc"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+html_theme = "alabaster"
