b28985bd36c403ac97d9054a318f1d266901f939
Adrian Sampson <adrian@radbox.org>
2024-04-26 20:02:51 -0400

    Run Black on benchmark stuff

--- bench/sizes.py
+++ bench/sizes.py
@@ -4,29 +4,29 @@ import os
 
 GRAPHS_TOML = os.path.join(os.path.dirname(__file__), "graphs.toml")
 SIZE_NAMES = {
-    0: '',
-    3: 'k',
-    6: 'M',
-    9: 'G',
-    12: 'T',
+    0: "",
+    3: "k",
+    6: "M",
+    9: "G",
+    12: "T",
 }
 
 
 def fmt_size(count):
     for scale, name in reversed(SIZE_NAMES.items()):
-        unit = 10 ** scale
+        unit = 10**scale
         if count > unit:
-            return '{:.0f}{}B'.format(count / unit, name)
+            return "{:.0f}{}B".format(count / unit, name)
 
 
 def show_sizes():
-    with open(GRAPHS_TOML, 'rb') as f:
+    with open(GRAPHS_TOML, "rb") as f:
         graphs_data = tomllib.load(f)
 
     for category, graphs in graphs_data.items():
         for name, url in graphs.items():
             res = requests.head(url)
-            length = int(res.headers['Content-Length'])
+            length = int(res.headers["Content-Length"])
             print(category, name, fmt_size(length))
 
 
