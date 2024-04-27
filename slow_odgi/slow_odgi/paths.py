ef91da1b6659c006dbe82779dad8fe599f7afc8c
Anshuman Mohan <anshumanmohan@live.com>
2024-04-27 11:08:36 -0400

    Simplify paths.py

--- slow_odgi/slow_odgi/paths.py
+++ slow_odgi/slow_odgi/paths.py
@@ -1,22 +1,14 @@
 import sys
-import random
 import mygfa
 
 
-def paths(graph: mygfa.Graph, droprate: int = 0) -> mygfa.Graph:
-    """Print the names of the paths found in `graph`.
-    The droprate represents the percentage of paths to drop.
-    """
+def paths(graph: mygfa.Graph) -> mygfa.Graph:
+    """Print the names of the paths found in `graph`."""
     pathnames = list(graph.paths.keys())
-    if droprate > 0:
-        random.seed(4)
-        pathnames[:] = random.sample(
-            pathnames, int((100 - droprate) / 100 * len(pathnames))
-        )
     for name in pathnames:
         print(name)
     return graph
 
 
 if __name__ == "__main__":
-    paths(mygfa.Graph.parse(open(sys.argv[1], "r", encoding="utf-8")), int(sys.argv[2]))
+    paths(mygfa.Graph.parse(open(sys.argv[1], "r", encoding="utf-8")))
