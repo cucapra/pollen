d1cf3cccdf95d0c0aa58ea768e6f54b9d87c1638
Adrian Sampson <adrian@radbox.org>
2024-05-02 11:15:54 -0400

    Expose loading of FlatGFA files

--- flatgfa-py/example.py
+++ flatgfa-py/example.py
@@ -1,5 +1,9 @@
 import flatgfa
-g = flatgfa.parse("../tests/k.gfa")
+
+g = flatgfa.parse("../bench/graphs/test.k.gfa")
 print(g.segments[2])
 for seg in g.segments:
     print(seg.name, seg.id, seg.sequence())
+
+g = flatgfa.load("../bench/graphs/test.k.flatgfa")
+print(g.segments[2].sequence())
