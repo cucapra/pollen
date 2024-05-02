15c2c94481e48d47bfb5b18ebc760b66650eef1a
Adrian Sampson <adrian@radbox.org>
2024-05-01 23:32:28 -0700

    Actually access a sequence

--- flatgfa-py/src/lib.rs
+++ flatgfa-py/src/lib.rs
@@ -1,5 +1,6 @@
-use flatgfa::flatgfa::{HeapStore, Segment};
+use flatgfa::flatgfa::{GFABuilder, HeapStore, Segment};
 use pyo3::prelude::*;
+use pyo3::types::PyBytes;
 
 #[pyfunction]
 fn parse(filename: &str) -> PyFlatGFA {
@@ -8,20 +9,33 @@ fn parse(filename: &str) -> PyFlatGFA {
     PyFlatGFA(store)
 }
 
-#[pyclass]
+#[pyclass(frozen)]
 #[pyo3(name = "FlatGFA")]
 struct PyFlatGFA(HeapStore);
 
 #[pymethods]
 impl PyFlatGFA {
-    fn get_a_seg(&self) -> PySegment {
-        PySegment(self.0.segs[0])
+    fn get_a_seg(self_: Py<Self>) -> PySegment {
+        let s = self_.get().0.segs[0];
+        PySegment { gfa: self_, seg: s }
     }
 }
 
-#[pyclass]
+#[pyclass(frozen)]
 #[pyo3(name = "Segment")]
-struct PySegment(Segment);
+struct PySegment {
+    gfa: Py<PyFlatGFA>,
+    seg: Segment,
+}
+
+#[pymethods]
+impl PySegment {
+    fn get_seq<'py>(&self, py: Python<'py>) -> Bound<'py, PyBytes> {
+        let view = self.gfa.get().0.view();
+        let seq = view.get_seq(&self.seg);
+        PyBytes::new_bound(py, seq) // TK Can we avoid this copy?
+    }
+}
 
 #[pymodule]
 #[pyo3(name = "flatgfa")]
