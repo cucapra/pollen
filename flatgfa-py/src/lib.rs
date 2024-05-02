0ec3bdfda0667d0edb8018647853ce8a8266517e
Adrian Sampson <adrian@radbox.org>
2024-05-01 21:17:12 -0700

    Sketch a lookup thing?

--- flatgfa-py/src/lib.rs
+++ flatgfa-py/src/lib.rs
@@ -1,15 +1,27 @@
-use flatgfa::flatgfa::HeapStore;
+use flatgfa::flatgfa::{HeapStore, Segment};
 use pyo3::prelude::*;
 
 #[pyfunction]
-fn parse(filename: &str) -> Graph {
+fn parse(filename: &str) -> PyFlatGFA {
     let file = flatgfa::file::map_file(filename);
     let store = flatgfa::parse::Parser::for_heap().parse_mem(file.as_ref());
-    Graph(store)
+    PyFlatGFA(store)
 }
 
 #[pyclass]
-struct Graph(HeapStore);
+#[pyo3(name = "FlatGFA")]
+struct PyFlatGFA(HeapStore);
+
+#[pymethods]
+impl PyFlatGFA {
+    fn get_a_seg(&self) -> PySegment {
+        PySegment(self.0.segs[0])
+    }
+}
+
+#[pyclass]
+#[pyo3(name = "Segment")]
+struct PySegment(Segment);
 
 #[pymodule]
 #[pyo3(name = "flatgfa")]
