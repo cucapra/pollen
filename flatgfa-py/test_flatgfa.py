8e4ee0c4aa4d4597b63121f2ebf49515e3a7363a
Adrian Sampson <adrian@radbox.org>
2024-05-14 08:03:24 -0400

    Comment the tests

--- flatgfa-py/test_flatgfa.py
+++ flatgfa-py/test_flatgfa.py
@@ -22,24 +22,35 @@ def gfa():
 
 
 def test_segs(gfa):
+    # `gfa.segments` acts like a list.
     assert len(gfa.segments) == 4
     seg = gfa.segments[0]
+
+    # An individual segment exposes its name and nucleotide sequence.
     assert seg.name == 1
     assert seg.sequence() == b"CAAATAAG"
+
+    # You can also pull out the entire sequence of segments.
     seg = list(gfa.segments)[2]
     assert seg.name == 3
 
 
 def test_paths(gfa):
+    # `gfa.paths` similarly acts like a list.
     assert len(gfa.paths) == 2
     assert len(list(gfa.paths)) == 2
+
+    # Individual paths expose their name (a bytestring).
     path = gfa.paths[0]
     assert path.name == b"one"
 
 
 def test_path_steps(gfa):
+    # When you get a path, the path itself acts as a list of steps (handles).
     path = gfa.paths[1]
     assert len(path) == 4
     step = list(path)[0]
+
+    # A step (handle) is a reference to a segment and an orientation.
     assert step.segment.name == 1
     assert step.is_forward
