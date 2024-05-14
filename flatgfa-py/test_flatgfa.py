c160c6d5c02b2efd9823e6f472b5ef2daa5c395a
Adrian Sampson <adrian@radbox.org>
2024-05-14 07:59:52 -0400

    Parse GFA from an embedded string
    Seems a little more manageable.

--- flatgfa-py/test_flatgfa.py
+++ flatgfa-py/test_flatgfa.py
@@ -1,16 +1,28 @@
 import pytest
 import flatgfa
 
-TEST_GFA = "../tests/k.gfa"
+TINY_GFA = b"""
+H	VN:Z:1.0
+S	1	CAAATAAG
+S	2	AAATTTTCTGGAGTTCTAT
+S	3	TTG
+S	4	CCAACTCTCTG
+P	one	1+,2+,4-	*
+P	two	1+,2+,3+,4-	*
+L	1	+	2	+	0M
+L	2	+	4	-	0M
+L	2	+	3	+	0M
+L	3	+	4	-	0M
+""".strip()
 
 
 @pytest.fixture
 def gfa():
-    return flatgfa.parse(TEST_GFA)
+    return flatgfa.parse_bytes(TINY_GFA)
 
 
 def test_segs(gfa):
-    assert len(gfa.segments) == 15
+    assert len(gfa.segments) == 4
     seg = gfa.segments[0]
     assert seg.name == 1
     assert seg.sequence() == b"CAAATAAG"
@@ -22,12 +34,12 @@ def test_paths(gfa):
     assert len(gfa.paths) == 2
     assert len(list(gfa.paths)) == 2
     path = gfa.paths[0]
-    assert path.name == b"x"
+    assert path.name == b"one"
 
 
 def test_path_steps(gfa):
     path = gfa.paths[1]
-    assert len(path) == 10
+    assert len(path) == 4
     step = list(path)[0]
     assert step.segment.name == 1
     assert step.is_forward
