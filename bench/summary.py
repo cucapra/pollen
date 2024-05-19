98e9901e4b74a0e7bd9af6d6f62203cd234c5365
Adrian Sampson <adrian@radbox.org>
2024-05-19 15:19:42 -0400

    Calculate ratios

--- bench/summary.py
+++ bench/summary.py
@@ -9,16 +9,24 @@ def summary():
     for row in reader:
         by_graph[row["graph"]][row["cmd"]] = row
 
+    # Guess a suitable baseline by taking the fastest time on the first graph.
+    first_res = next(iter(by_graph.values()))
+    min_row = min(first_res.values(), key=lambda r: r["mean"])
+    baseline = min_row["cmd"]
+
     for graph, cmds in by_graph.items():
+        baseline_time = float(cmds[baseline]["mean"])
+
         print(graph)
         for cmd, row in cmds.items():
             mean = float(row["mean"])
             stddev = float(row["stddev"])
+            ratio = mean / baseline_time
 
             if mean > 80:
                 mins = int(mean / 60)
                 secs = int(mean % 60)
-                print(f"  {cmd}: {mins}m{secs}s ± {stddev:.1f}")
+                print(f"  {cmd}: {mins}m{secs}s ± {stddev:.1f}", end='')
             else:
                 if mean < 0.2:
                     mean *= 1000
@@ -26,7 +34,9 @@ def summary():
                     unit = "ms"
                 else:
                     unit = "s"
-                print(f"  {cmd}: {mean:.1f} ± {stddev:.1f} {unit}")
+                print(f"  {cmd}: {mean:.1f} ± {stddev:.1f} {unit}", end='')
+
+            print(f" ({ratio:.1f}× {baseline})")
 
 
 if __name__ == "__main__":
