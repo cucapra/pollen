dabc0ba4d1ac45c84d1f55b7f509f9f8a5f95d8e
Adrian Sampson <adrian@radbox.org>
2024-12-29 15:27:45 -0500

    Reuse a single BitVec
    Seems to have no impact on performance, but it makes me feel better
    anyway.

--- flatgfa/src/ops/depth.rs
+++ flatgfa/src/ops/depth.rs
@@ -5,8 +5,9 @@ pub fn depth(gfa: &flatgfa::FlatGFA) -> (Vec<usize>, Vec<usize>) {
     let mut depths = vec![0; gfa.segs.len()]; // The number of paths that traverse each segment.
     let mut uniq_depths = vec![0; gfa.segs.len()]; // The number of distinct paths that traverse them.
 
+    let mut seen = BitVec::from_elem(gfa.segs.len(), false); // Has the current path traversed each segment?
     for path in gfa.paths.all().iter() {
-        let mut seen = BitVec::from_elem(gfa.segs.len(), false); // Has this path traversed this segment?
+        seen.clear();
         for step in &gfa.steps[path.steps] {
             let seg_id = step.segment().index();
             depths[seg_id] += 1;
