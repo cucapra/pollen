a5399d7598a44f96a54d3eba768e340cfb6fe8e6
mt-xing <mt-xing@users.noreply.github.com>
2025-03-11 14:38:20 -0400

    Undo example

--- flatgfa-py/example.py
+++ flatgfa-py/example.py
@@ -1,12 +1,12 @@
 import flatgfa
 from collections import Counter
 
-graph = flatgfa.load("chr6.flatgfa")
-print(graph.size)
-# graph.print_gaf_lookup("ch6.gaf")
+graph = flatgfa.parse("../tests/k.gfa")
+depths = Counter()
+for path in graph.paths:
+    for step in path:
+        depths[step.segment.id] += 1
 
-for read in graph.test_gaf("ch6.gaf"):
-    for event in read:
-        print(event.handle)
-        print(event.range)
-        print(event.get_seq(graph))
+print("#node.id\tdepth")
+for seg in graph.segments:
+    print("{}\t{}".format(seg.name, depths[seg.id]))
