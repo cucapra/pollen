3959fa9d351ca11b7fc662424159e23321c4cca2
John Palsberg <jlp377@cornell.edu>
2025-03-26 16:52:02 -0400

    Flesh out the packseq file more

--- flatgfa/src/packedseq.rs
+++ flatgfa/src/packedseq.rs
@@ -1,3 +1,5 @@
+use std::ops::Index;
+use std::fmt;
 static A: u8 = 0;
 static C: u8 = 1;
 static G: u8 = 2;
@@ -5,7 +7,6 @@ static T: u8 = 3;
 
 struct PackedVec {
     data: Vec<u8>,
-    high_nibble_start: bool,
     high_nibble_end: bool,
 }
 
@@ -13,12 +14,18 @@ impl PackedVec {
     fn new() -> Self {
         PackedVec {
             data: Vec::new(),
-            high_nibble_start: false,
             high_nibble_end: false,
         }
     }
     pub fn push(&mut self, value: u8) {
-        self.data.push(value);
+        if self.high_nibble_end {
+            self.data.push(value);
+            self.high_nibble_end = false;
+        } else {
+            let last_index = self.data.len() - 1;
+            self.data[last_index] = (value << 4) | self.data[last_index];
+            self.high_nibble_end = true;
+        }
     }
 }
 
@@ -30,10 +37,48 @@ impl Index<usize> for PackedVec {
     }
 }
 
+impl fmt::Display for PackedVec {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "[")?;
+        for (i, &item) in self.data.iter().enumerate() {
+            if i != 0 {
+                write!(f, ", ")?;
+            }
+            write!(f, "{}", item)?;
+        }
+        write!(f, "]")
+    }
+}
+
+struct PackedVecIterator<'a> {
+    data: &'a Vec<u8>,
+    cur_index: usize,
+    cur_high_nibble: bool,
+}
+
+impl<'a> Iterator for PackedVecIterator<'a> {
+
+    type Item = u8;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.cur_index >= self.data.len() {
+            return None; 
+        }
+        let result = if self.cur_high_nibble {
+            (self.data[self.cur_index] >> 4) & 0b00001111u8  
+        }
+        else {
+            self.data[self.cur_index]& 0b00001111u8  
+        };
+        self.cur_index += 1;
+        self.cur_high_nibble = !self.cur_high_nibble;
+        Some(result)
+    }
+}
+
 struct PackedSlice<'a> {
     data_ref: &'a Vec<u8>,
-    high_nibble_start: bool,
-    high_nibble_end: bool,
+    span: Span
 }
 
 struct Span {
@@ -41,21 +86,25 @@ struct Span {
     end: u8,
 }
 
-fn get_seq(seq: &Sequence, span: Span) -> Vec<u8> {
+fn get_seq(compressed_data: &Vec<u8> , span: Span)  -> Vec<u8> {
     let mut arr: Vec<u8> = Vec::with_capacity((span.end - span.start).into());
     let mut j = 0;
     for i in span.start..=span.end {
         if i % 2 == 1 {
-            arr.push((seq.data[i as usize / 2] & 0b11110000u8) >> 4);
+            arr.push((compressed_data[i as usize / 2] & 0b11110000u8) >> 4);
         } else {
-            arr.push(seq.data[i as usize / 2] & 0b00001111u8);
+            arr.push(compressed_data[i as usize / 2] & 0b00001111u8);
         }
         j += 1;
     }
     return arr;
 }
 
-fn create_vec(arr: Vec<u8>) -> Sequence {
+fn get_vec_seq(seq: &PackedVec, span: Span) -> Vec<u8> {
+    return get_seq(&seq.data, span);
+}
+
+fn create_vec(arr: Vec<u8>) -> PackedVec {
     let mut high_nibble = false;
     if arr.len() % 2 == 1 {
         high_nibble = true;
@@ -68,7 +117,7 @@ fn create_vec(arr: Vec<u8>) -> Sequence {
     let mut j = 0;
     let mut low = true;
     for i in 0..arr.len() {
-        if (low == true) {
+        if low == true {
             new_data.push(arr[i]);
             low = false;
         } else {
@@ -79,32 +128,21 @@ fn create_vec(arr: Vec<u8>) -> Sequence {
     }
     return PackedVec {
         data: new_data,
-        high_nibble_start: false,
         high_nibble_end: high_nibble,
     };
 }
 
-fn create_slice(vec: PackedVec, span: Span) -> PackedSlice {
-    return PackedSlice {};
+fn create_slice<'a>(vec: PackedVec, span: Span) -> PackedSlice<'a> {
+    return PackedSlice{&PackedVec.data, span};
 }
 
-fn print_arr(arr: Vec<u8>) {
-    let new_arr: Vec<char> = arr
-        .into_iter()
-        .map(|e| match e {
-            0 => 'A',
-            1 => 'C',
-            2 => 'T',
-            3 => 'G',
-            _ => ' ',
-        })
-        .collect();
-    println!("{:?}", new_arr);
+fn get_slice_seq<'a>(slice: PackedSlice<'a>, span: Span) -> Vec<u8> {
+    return get_seq(&slice.data_ref, span);
 }
 
 fn main() {
     let span = Span { start: 0, end: 4 };
-    let seq = create_seq(vec![A, C, G, T, A]);
-    let new_arr = get_seq(&seq, span);
-    print_arr(new_arr);
+    let mut seq = create_vec(vec![A, C, G, T, A]);
+    seq.push(A);
+    println!("{}", seq);
 }
