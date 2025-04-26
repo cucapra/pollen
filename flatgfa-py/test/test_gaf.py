211f803aad0dd9e66c9f8daa4dedf9a3546b416b
Adrian Sampson <adrian@radbox.org>
2025-04-26 12:19:18 -0400

    Test `.range` attribute of GAF events

--- flatgfa-py/test/test_gaf.py
+++ flatgfa-py/test/test_gaf.py
@@ -6,11 +6,21 @@ TEST_GFA = str(TEST_DIR / "tiny.gfa")
 TEST_GAF = str(TEST_DIR / "tiny.gaf")
 
 
-def test_gaf():
+def test_gaf_seqs():
     gfa = flatgfa.parse(TEST_GFA)
     gaf = gfa.all_reads(TEST_GAF)
     seqs = ["".join(e.sequence() for e in line) for line in gaf]
     assert seqs == [
         "AAGAAATTTTCT",
         "GAAATTTTCTGGAGTTCTAT",
+    ]
+
+
+def test_gaf_ranges():
+    gfa = flatgfa.parse(TEST_GFA)
+    gaf = gfa.all_reads(TEST_GAF)
+    ranges = [[e.range for e in line] for line in gaf]
+    assert ranges == [
+        [(5, 8), (0, 9), (1, 0)],
+        [(7, 8), (0, 18), (0, 0)],
     ]
