e97c09fe6030dfa18df27452d24f9a7159a965be
Adrian Sampson <adrian@radbox.org>
2025-04-26 12:12:33 -0400

    Better test file discovery

--- flatgfa-py/test/test_gaf.py
+++ flatgfa-py/test/test_gaf.py
@@ -1,11 +1,15 @@
-import pytest
+import pathlib
 import flatgfa
+
+TEST_DIR = pathlib.Path(__file__).parent
+TEST_GFA = str(TEST_DIR / "tiny.gfa")
+TEST_GAF = str(TEST_DIR / "tiny.gaf")
 
 
 def test_gaf():
-    gfa = flatgfa.parse("test/tiny.gfa")
-    gaf = gfa.test_gaf("test/tiny.gaf")
-    seqs = ["".join(e.get_seq() for e in line) for line in gaf]
+    gfa = flatgfa.parse(TEST_GFA)
+    gaf = gfa.all_reads(TEST_GAF)
+    seqs = ["".join(e.sequence() for e in line) for line in gaf]
     assert seqs == [
         "AAGAAATTTTCT",
         "GAAATTTTCTGGAGTTCTAT",
