cf9b7b591b4d62de7a800106a00147e214f65ac9
John Palsberg <jlp377@cornell.edu>
2025-06-13 00:23:21 -0700

    Clean up code for push

--- flatgfa/src/cli/main.rs
+++ flatgfa/src/cli/main.rs
@@ -43,8 +43,6 @@ enum Command {
     Chop(cmds::Chop),
     GafLookup(cmds::GAFLookup),
     Bench(cmds::Bench),
-    SeqImport(cmds::SeqImport),
-    SeqExport(cmds::SeqExport),
 }
 
 fn main() -> Result<(), &'static str> {
@@ -136,12 +134,6 @@ fn main() -> Result<(), &'static str> {
         }
         Some(Command::Bench(sub_args)) => {
             cmds::bench(sub_args);
-        }
-        Some(Command::SeqImport(sub_args)) => {
-            cmds::seq_import(sub_args);
-        }
-        Some(Command::SeqExport(sub_args)) => {
-            cmds::seq_export(sub_args);
         }
         None => {
             // Just emit the GFA or FlatGFA file.
--- flatgfa/src/test_export_import.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cargo run -p flatgfa --bin flatgfa -- export test_file
-cargo run -p flatgfa --bin flatgfa -- import test_file
-rm -f test_file
