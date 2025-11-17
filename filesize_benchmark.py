5af1ae9e9fd458cc76d920e498d7f6256ee6039f
John Palsberg <jlp377@cornell.edu>
2025-11-17 02:24:20 -0500

    Add bencher yml file and bencher python script

---
+++ filesize_benchmark.py
@@ -0,0 +1,27 @@
+import sys
+import os
+import json
+import subprocess
+
+subprocess.run(["cargo", "build", "--release"])
+subprocess.run(["fgfa", "-I", "tests/DRB1-3123.gfa","-o", "filesize_benchmark.txt"]) 
+
+
+size_bytes = os.path.getsize("filesize_benchmark.txt")
+
+bencher_json = {
+    "benchmarks": [
+        {
+            "name": "filesize",
+            "measurements": [
+                {
+                    "value": size_bytes,
+                    "unit": "bytes"
+                }
+            ]
+        }
+    ]
+}
+
+
+
