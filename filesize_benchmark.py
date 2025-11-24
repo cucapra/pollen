a0db4d7807fd261f7528d69a332c95dd2c7ad24f
John Palsberg <jlp377@cornell.edu>
2025-11-24 01:54:34 -0500

    Add Bencher latency benchmark

--- filesize_benchmark.py
+++ filesize_benchmark.py
@@ -3,14 +3,14 @@ import os
 import json
 import subprocess
 
-subprocess.run(["cargo", "build", "--release"])
-subprocess.run(["fgfa", "-I", "tests/DRB1-3123.gfa","-o", "filesize_benchmark.txt"]) 
+subprocess.run(["cargo", "build", "--release"], check = True)
+subprocess.run(["fgfa", "-I", "tests/DRB1-3123.gfa","-o", "filesize_benchmark.txt"], check = True) 
 
 
 size_bytes = os.path.getsize("filesize_benchmark.txt")
 
 bencher_json = {
-  "benchmark_name": {
+  "FlatGFA File Size": {
     "file-size": {
       "value": size_bytes, 
     }
---
+++ latency_benchmark.py
@@ -0,0 +1,40 @@
+import sys
+import time
+import os
+import json
+import subprocess
+
+
+subprocess.run(["cargo", "build", "--release"], check = True)
+
+start_time = 0.0
+end_time = 0.0
+avg_time = 0.0
+ 
+for i in range(10):
+
+  start_time = time.time()
+
+  subprocess.run(["fgfa", "-I", "tests/DRB1-3123.gfa", "extract", "-n", "-c", "3"], check = True) 
+
+  end_time = time.time()
+
+  avg_time += (end_time - start_time) * 1000 # ms
+
+
+avg_time /= 10
+
+
+bencher_json = {
+  "FlatGFA Extract Latency": {
+    "latency": {
+      "value": avg_time
+    }
+  }
+}
+
+print(json.dumps(bencher_json))
+
+
+
+
