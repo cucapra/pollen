f8b8137f6ecf77a9b09f98b04aadd564dfb8acc4
John Palsberg <jlp377@cornell.edu>
2025-11-24 02:11:37 -0500

    Revert file size test find bug

--- filesize_benchmark.py
+++ filesize_benchmark.py
@@ -3,8 +3,8 @@ import os
 import json
 import subprocess
 
-subprocess.run(["cargo", "build", "--release"], check = True)
-subprocess.run(["fgfa", "-I", "tests/DRB1-3123.gfa","-o", "filesize_benchmark.txt"], check = True) 
+subprocess.run(["cargo", "build", "--release"])
+subprocess.run(["fgfa", "-I", "tests/DRB1-3123.gfa","-o", "filesize_benchmark.txt"]) 
 
 
 size_bytes = os.path.getsize("filesize_benchmark.txt")
