b80f2ade7fa9254deabbd7522b5fe3ac62b05561
John Palsberg <jlp377@cornell.edu>
2026-04-07 13:09:15 -0400

    Add an annotated benchmark file

--- bench/benchmark_web_annotated.py
+++ bench/benchmark_web_annotated.py
@@ -8,9 +8,20 @@ import tomllib
 import gzip
 import shutil
 
+# Modify these variables to avoid file conflicts and create unique command names
+results = "filesize_benchmark.txt" 
+test_id = "" 
+normalization_file = "bench/normalization.toml"
+command_1 = "extract"
+command_2 = "chop"
+command_3 = "depth" 
+
 # Parse the GFA URLs from graphs.toml
 with open("bench/graphs.toml", "rb") as f:
     toml_graphs = tomllib.load(f)
+
+# The following variables extract specific sections of the graphs.toml file. If you are 
+# using standard GFA files for your test, no need to modify these
 
 hprc_dict = dict(toml_graphs["hprc"])
  
@@ -26,7 +37,7 @@ med_files = [hprc_dict["chr20"], hprc_dict["chrX"], gont_dict["chr16"]]
 
 big_files = [hprc_dict["chrY"], hprc_dict["chr1"], hprc_dict["chr10"]]
 
-results = "filesize_benchmark.txt"
+
 
 # Download a GFA file from the internet
 def download_file(target_name, web_file):
@@ -49,40 +60,41 @@ def download_file(target_name, web_file):
       subprocess.run(["curl", "-o", target_name, web_file],
               check = True) 
 
-# Run a single latency test      
+# Run a single latency test
+# Modify the indicated lines to test your own comamand
 def test(command, test_file_name, num_iter):
-  if command == "extract":
+  if command == command_1:  
     with open(os.devnull, "w") as devnull:
       start_time = time.time()
       for _ in range(num_iter):
         subprocess.run(["fgfa", "-I", test_file_name, "extract", "-n", "3", "-c", "3"], stdout=devnull,
             stderr=devnull,
-            check=True) 
+            check=True) # Note: Replace with the command you want to test
       end_time = time.time()
       return ((end_time - start_time) * 1000) / num_iter
         
-  elif command == "chop":
+  elif command == command_2: 
     with open(os.devnull, "w") as devnull:
       start_time = time.time()
       for _ in range(num_iter):
         subprocess.run(["fgfa", "-I", test_file_name, "chop", "-c", "3", "-l"], stdout=devnull,
             stderr=devnull,
-            check=True) 
+            check=True) # Note: Replace with the command you want to test
       end_time = time.time()
       return ((end_time - start_time) * 1000) / num_iter
 
-  elif command == "depth":
+  elif command == command_3: 
     with open(os.devnull, "w") as devnull:
       start_time = time.time()
       for _ in range(num_iter):
         subprocess.run(["fgfa", "-I", test_file_name, "depth"], stdout=devnull,
             stderr=devnull,
-            check=True) 
+            check=True) # Note: Replace with the command you want to test
       end_time = time.time()
       return ((end_time - start_time) * 1000) / num_iter
   return 0.0
   
-# Run the benchmark across all test files
+# Run the benchmarks across all test files
 def benchmark(test_config):
   del_cond = ""
   norm_cond = ""
@@ -118,25 +130,24 @@ def benchmark(test_config):
   
   # Set number of test iterations
   if not iter_count == -1:
-   num_iter = iter_count
+    num_iter = iter_count
   
   i = 0
-  total_time = 0.0
-  extract_time = 0.0
-  chop_time = 0.0
-  depth_time = 0.0
+  command_1_time = 0.0
+  command_2_time = 0.0
+  command_3_time = 0.0
   size_bytes_avg = 0
 
   # Run a test for each file in the set
   for file in test_files:
-    test_file_name = f"tests/{test_config}_{i}.gfa"
+    test_file_name = f"tests/{test_config}_{i}_{test_id}.gfa"
     download_file(test_file_name, file)
     subprocess.run(["fgfa", "-I", test_file_name, "-o", results],
                   check = True) 
     size_bytes_avg += os.path.getsize(results)
-    extract_time += test("extract", test_file_name, num_iter)
-    chop_time += test("chop", test_file_name, num_iter)
-    depth_time += test("depth", test_file_name, num_iter)
+    command_1_time += test(command_1, test_file_name, num_iter)
+    command_2_time += test(command_2, test_file_name, num_iter)
+    command_3_time += test(command_3, test_file_name, num_iter)
     subprocess.run(["rm", "-rf", results], check = True) 
 
     # Delete test files if flag set
@@ -146,29 +157,29 @@ def benchmark(test_config):
   if (norm_cond == "norm"):
 
     # Write new normalization values
-    with open("bench/normalization.toml", "w") as f:
+    with open(normalization_file, "w") as f:
       f.write("[normalization_factors]\n")
-      f.write(f"extract = {extract_time}\n")
-      f.write(f"chop = {chop_time}\n")
-      f.write(f"depth = {depth_time}\n")
+      f.write(f"{command_1} = {command_1_time}\n")
+      f.write(f"{command_2}= {command_2_time}\n")
+      f.write(f"{command_3} = {command_3_time}\n")
     return (1.0, size_bytes_avg)
   else:
 
     # Read normalization values
-    with open("bench/normalization.toml", "rb") as f:
+    with open(normalization_file, "rb") as f:
       data = tomllib.load(f)
-    extract_norm = data["normalization_factors"]["extract"]
-    chop_norm = data["normalization_factors"]["chop"]
-    depth_norm = data["normalization_factors"]["depth"]
+    command_1_norm = data["normalization_factors"][command_1]
+    command_2_norm = data["normalization_factors"][command_2]
+    command_3_norm = data["normalization_factors"][command_3]
 
     # Normalize values
-    extract_time /= extract_norm
-    chop_time /= chop_norm
-    depth_time /= depth_norm
+    command_1_time /= command_1_norm
+    command_2_time /= command_2_norm
+    command_3_time /= command_3_norm
 
     # Return the harmonic mean
     size_bytes_avg /= len(test_files)
-    return (3 / ((1 / extract_time) + (1 / chop_time) + (1 / depth_time)), size_bytes_avg / 1000.0)
+    return (3 / ((1 / command_1_time) + (1 / command_2_time) + (1 / command_3_time)), size_bytes_avg / 1000.0)
 
 # Read the desired test file set from command-line input
 test_config = ""
@@ -182,6 +193,7 @@ bench_results = benchmark(test_config)
 
 # Output the benchmark results, either in a Bencher JSON format, or a standard 
 # command-line format
+# Modify the print statements to print what makes sense for what you're testing
 if "bencher" in test_config:
   bencher_json = {
     "FlatGFA Benchmark Results": {
@@ -204,5 +216,15 @@ else:
     print(f"Average File Size: {round(bench_results[1], 2)} KB")
   
 
-# Command format: python bench/benchmark_web.py [size](_bencher/_latency/_filesize) [run_count] (del/_) (norm)
+# Command format: python bench/benchmark_web_annotated.py [size](_bencher/_latency/_filesize) [run_count] (del/_) (norm)
 # () = optional, [] = replace with value  
+ 
+# Explanation: The user selects the variant of the test they want to run with a flag.
+# For example, "python bench/latency_benchmark_web.py mini_latency 10" tests the mini GFA file set on the latency test
+# for 10 iterations, while replacing "mini_latency" with "mini_filesize" runs the filesize test instead. 
+# Including the "del" flag delete the downloaded GFA files after the test runs
+#   (Example usage: "python bench/latency_benchmark_web.py mini_latency 10 del")
+# Including the "norm" flag resets the normalization constants. 
+#    (Example usage: "python bench/latency_benchmark_web.py mini_latency 10 _ norm")
+ 
+# It's important to list the flags in the correct order, and in the correct position 
