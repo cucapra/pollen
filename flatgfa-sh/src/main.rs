af060887d13b67f4db0abdd537dd5292ab25ffa9
Adrian Sampson <adrian@radbox.org>
2026-04-21 15:30:17 -0400

    Start IR data structures

--- flatgfa-sh/src/main.rs
+++ flatgfa-sh/src/main.rs
@@ -1,4 +1,6 @@
-use flash::parser::Node;
+use std::os::macos::raw::stat;
+
+use flash::parser::{Node, Redirect};
 
 fn parse(input: &str) -> Node {
     // Following the example from the flash README.
@@ -9,7 +11,69 @@ fn parse(input: &str) -> Node {
     parser.parse_script()
 }
 
+#[derive(Debug)]
+enum GraphResource {
+    File(String),
+}
+
+#[derive(Debug)]
+struct DepthOp {
+    input: GraphResource,
+    path: Option<String>,
+}
+
+#[derive(Debug)]
+enum Op {
+    Depth(DepthOp),
+}
+
+#[derive(Debug)]
+struct Program {
+    ops: Vec<Op>,
+}
+
+fn cmd_to_ir(name: String, args: Vec<String>, redirects: Vec<Redirect>) -> Op {
+    if name == "odgi" {
+        if args[0] == "depth" {
+            dbg!(args);
+            dbg!(redirects);
+            Op::Depth(DepthOp {
+                input: GraphResource::File("hi".to_string()),
+                path: None,
+            })
+        } else {
+            unimplemented!("unsupported odgi subcommand");
+        }
+    } else {
+        unimplemented!("only odgi commands are supported");
+    }
+}
+
+fn script_to_ir(shell: Node) -> Program {
+    match shell {
+        Node::List {
+            statements,
+            operators,
+        } => {
+            let ops: Vec<_> = statements
+                .into_iter()
+                .map(|statement| match statement {
+                    Node::Command {
+                        name,
+                        args,
+                        redirects,
+                    } => cmd_to_ir(name, args, redirects),
+                    _ => unimplemented!(),
+                })
+                .collect();
+            Program { ops }
+        }
+        _ => unimplemented!(),
+    }
+}
+
 fn main() {
-    let line = parse("odgi depth -i chr8.pan.og -r chm13#chr8");
-    dbg!(line);
+    let shell = parse("odgi depth -i chr8.pan.og -r 'chm13#chr8'");
+    dbg!(&shell);
+    dbg!(&script_to_ir(shell));
 }
