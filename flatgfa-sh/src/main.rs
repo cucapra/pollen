a186da79c0af3193ee8e2caf9fdcbaf18064cde3
Adrian Sampson <adrian@radbox.org>
2026-04-21 22:36:26 -0400

    -c option, like a real shell

--- flatgfa-sh/src/main.rs
+++ flatgfa-sh/src/main.rs
@@ -6,7 +6,7 @@ mod pretty;
 use rustyline::DefaultEditor;
 use rustyline::error::ReadlineError;
 
-fn run_line(line: &str, pretend: bool) {
+fn run_shell(line: &str, pretend: bool) {
     let shell = parse::parse_sh(line);
     let prog = parse::sh_to_ir(shell);
     if pretend {
@@ -20,7 +20,7 @@ fn repl(pretend: bool) -> rustyline::Result<()> {
     let mut rl = DefaultEditor::new()?;
     loop {
         match rl.readline("$ ") {
-            Ok(line) => run_line(&line, pretend),
+            Ok(line) => run_shell(&line, pretend),
             Err(ReadlineError::Interrupted) => break,
             Err(ReadlineError::Eof) => break,
             Err(err) => {
@@ -35,5 +35,10 @@ fn repl(pretend: bool) -> rustyline::Result<()> {
 fn main() {
     let mut args = pico_args::Arguments::from_env();
     let pretend = args.contains(["-p", "--pretend"]);
-    repl(pretend).unwrap();
+    let cmd: Option<String> = args.opt_value_from_str("-c").unwrap();
+    if let Some(cmd) = cmd {
+        run_shell(&cmd, pretend);
+    } else {
+        repl(pretend).unwrap();
+    }
 }
