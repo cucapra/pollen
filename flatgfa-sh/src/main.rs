963f480b122a548ad4709087b8c11960c598d431
Adrian Sampson <adrian@radbox.org>
2026-04-21 21:29:58 -0400

    Split parse module

--- flatgfa-sh/src/main.rs
+++ flatgfa-sh/src/main.rs
@@ -1,60 +1,12 @@
 mod ir;
+mod parse;
 
-use flash::parser::{Node, Redirect};
-use pico_args::Arguments;
 use rustyline::DefaultEditor;
 use rustyline::error::ReadlineError;
 
-fn parse(input: &str) -> Node {
-    // Following the example from the flash README.
-    use flash::lexer::Lexer;
-    use flash::parser::Parser;
-    let lexer = Lexer::new(input);
-    let mut parser = Parser::new(lexer);
-    parser.parse_script()
-}
-
-fn cmd_to_ir(name: String, args: Vec<String>, redirects: Vec<Redirect>) -> ir::Op {
-    if name == "odgi" {
-        let mut argp = Arguments::from_vec(args.into_iter().map(|s| s.into()).collect());
-        match argp.subcommand().unwrap().as_deref() {
-            Some("depth") => ir::Op::Depth(ir::DepthOp {
-                input: ir::GraphResource::File(argp.value_from_str("-i").unwrap()),
-                path: argp.opt_value_from_str("-r").unwrap(),
-            }),
-            _ => unimplemented!("unsupported odgi subcommand"),
-        }
-    } else {
-        unimplemented!("only odgi commands are supported");
-    }
-}
-
-fn script_to_ir(shell: Node) -> ir::Program {
-    match shell {
-        Node::List {
-            statements,
-            operators,
-        } => {
-            let ops: Vec<_> = statements
-                .into_iter()
-                .map(|statement| match statement {
-                    Node::Command {
-                        name,
-                        args,
-                        redirects,
-                    } => cmd_to_ir(name, args, redirects),
-                    _ => unimplemented!(),
-                })
-                .collect();
-            ir::Program { ops }
-        }
-        _ => unimplemented!(),
-    }
-}
-
 fn run_line(line: &str) {
-    let shell = parse(&line);
-    let prog = script_to_ir(shell);
+    let shell = parse::parse_sh(&line);
+    let prog = parse::script_to_ir(shell);
     prog.run();
 }
 
---
+++ flatgfa-sh/src/parse.rs
@@ -0,0 +1,50 @@
+use crate::ir;
+use flash::parser::{Node, Redirect};
+use pico_args::Arguments;
+
+pub fn parse_sh(input: &str) -> Node {
+    // Following the example from the flash README.
+    use flash::lexer::Lexer;
+    use flash::parser::Parser;
+    let lexer = Lexer::new(input);
+    let mut parser = Parser::new(lexer);
+    parser.parse_script()
+}
+
+pub fn cmd_to_ir(name: String, args: Vec<String>, redirects: Vec<Redirect>) -> ir::Op {
+    if name == "odgi" {
+        let mut argp = Arguments::from_vec(args.into_iter().map(|s| s.into()).collect());
+        match argp.subcommand().unwrap().as_deref() {
+            Some("depth") => ir::Op::Depth(ir::DepthOp {
+                input: ir::GraphResource::File(argp.value_from_str("-i").unwrap()),
+                path: argp.opt_value_from_str("-r").unwrap(),
+            }),
+            _ => unimplemented!("unsupported odgi subcommand"),
+        }
+    } else {
+        unimplemented!("only odgi commands are supported");
+    }
+}
+
+pub fn script_to_ir(shell: Node) -> ir::Program {
+    match shell {
+        Node::List {
+            statements,
+            operators,
+        } => {
+            let ops: Vec<_> = statements
+                .into_iter()
+                .map(|statement| match statement {
+                    Node::Command {
+                        name,
+                        args,
+                        redirects,
+                    } => cmd_to_ir(name, args, redirects),
+                    _ => unimplemented!(),
+                })
+                .collect();
+            ir::Program { ops }
+        }
+        _ => unimplemented!(),
+    }
+}
