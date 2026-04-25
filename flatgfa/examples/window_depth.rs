4a607951bab2b33655fca38debd3698d68f44307
Adrian Sampson <adrian@radbox.org>
2026-04-24 22:30:51 -0400

    Update a reference

--- flatgfa/examples/window_depth.rs
+++ flatgfa/examples/window_depth.rs
@@ -91,7 +91,7 @@ fn create_bed(
     let max_line = 40;
     let mut bed_file = memfile::map_new_file(bed_name, (num_windows * max_line) as u64);
     let mut offset = 0;
-    let depth = depth::depth(&flatgfa).0;
+    let depth = depth::seg_depth(&flatgfa).0;
     let mut windows = window_vec;
     if windows == Vec::new() {
         windows = compute_windows(path_len, window_size);
